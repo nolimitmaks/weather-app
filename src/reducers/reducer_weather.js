dc64e24b91d90229b06c6f640682fbfe94dc5a76
Maks Hlushek <nolimitmaks@users.noreply.github.com>
2020-10-15 11:51:37 -0700

    Update reducer_weather.js

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,17 +1,9 @@
 import {FETCH_WEATHER} from '../actions/index'
 
-
-
-export default async function(state = null, action) {
-
+export default function(state = [], action) {
     switch(action.type) {
         case FETCH_WEATHER:
-            return action.payload.data
+            return [action.payload.data, ...state]
     }
-
-    
-
-    console.log('action received ', action)
     return state
-
 }
