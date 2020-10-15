dc03a42076f0011eba5f7b4767abbb022cb0dd30
Maks Hlushek <nolimitmaks@users.noreply.github.com>
2020-10-15 11:51:26 -0700

    try redux thunk

--- src/index.js
+++ src/index.js
@@ -21,3 +21,53 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+
+
+
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux'
+// import store from './store'
+
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers'
+import ReduxPromise from 'redux-promise'
+
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// import { createStore, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk';
+// import rootReducer from './reducers';
+
+// const store = createStore(
+//   rootReducer,
+//   applyMiddleware(thunk),
+//   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+// );
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={createStoreWithMiddleware(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+   <Provider store={store}> 
+      <App />
+    </Provider>,
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+*/
