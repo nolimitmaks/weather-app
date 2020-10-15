0b61a87f154fbcc8da034a89d5cff3f31e0ff39f
Maks Hlushek <nolimitmaks@users.noreply.github.com>
2020-10-15 11:51:05 -0700

    apply redux promise to middleware

--- src/store.js
+++ src/store.js
@@ -1,8 +1,19 @@
-import { createStore } from 'redux'
+// import { createStore } from 'redux'
+// import rootReducer from './reducers'
+
+
+// export default createStore(
+//     rootReducer, /* preloadedState, */
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+
+
+
+import { createStore, applyMiddleware } from 'redux'
+import ReduxPromise from 'redux-promise'
+
 import rootReducer from './reducers'
 
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
 
-export default createStore(
-    rootReducer, /* preloadedState, */
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+export default createStoreWithMiddleware(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
