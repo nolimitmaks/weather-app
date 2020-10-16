96e96da3687d4bda128f47cd7f37136c182ffa8d
Maks Hlushek <nolimitmaks@users.noreply.github.com>
2020-10-16 05:20:25 -0700

    create weather-list container

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './containers/search_bar';
+import WeatherList from './containers/weather_list';
 
 function App() {
   return (
     <div className="App">
       <SearchBar /> 
+      <WeatherList />
     </div>
   );
 }
---
+++ src/containers/weather_list.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+class WeatherList extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.renderWeather = this.renderWeather.bind(this)
+    }
+
+    renderWeather(cityData) {
+        const name = cityData.city.name
+
+        return (
+            <tr key={name}>
+                <td>{name}</td>
+            </tr>
+        )
+    }
+
+    render() {
+        return (
+            <table className='table table-hover'>
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Pressure</th>
+                        <th>Humidity</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+function mapStateToProps({weather}) {
+    return {
+        weather
+    }
+}
+
+export default connect(mapStateToProps)(WeatherList)
