66a92b4776a6e18f540e8a7279d6a35fd7f5a8c2
Maks Hlushek <nolimitmaks@users.noreply.github.com>
2020-10-18 15:22:50 -0700

    Update weather_list.js

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
+import LineChart from '../components/line_chart'
+
 class WeatherList extends React.Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            temp: []
+        }
 
         this.renderWeather = this.renderWeather.bind(this)
     }
@@ -11,28 +17,43 @@ class WeatherList extends React.Component {
     renderWeather(cityData) {
         const name = cityData.city.name
 
+        console.log(cityData.city.name)
+
+        const temp = cityData.list.map(
+            e => e.main.temp
+        )
         return (
             <tr key={name}>
                 <td>{name}</td>
+
+                <td>
+                    <LineChart temp={temp} />
+                </td>
             </tr>
+            
         )
     }
 
     render() {
+        console.log(this.props.weather);
         return (
-            <table className='table table-hover'>
-                <thead>
-                    <tr>
-                        <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.props.weather.map(this.renderWeather)}
-                </tbody>
-            </table>
+            <div>
+                <table className='table table-hover'>
+                    <thead>
+                        <tr>
+                            <th>City</th>
+                            <th>Temperature</th>
+                            <th>Pressure</th>
+                            <th>Humidity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.weather.map(this.renderWeather)}
+                    </tbody>
+                </table>
+
+                
+            </div>
         )
     }
 }
